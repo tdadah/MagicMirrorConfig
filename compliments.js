1435d7f80ed79420ab6721c6aced14c8a271fc50
Tara <tara.dadah@gmail.com>
2023-01-31 10:16:57 -0500

    Create compliments.js

---
+++ compliments.js
@@ -0,0 +1,38 @@
+{
+  "anytime": ["Hey there sexy!"],
+  "morning": [
+    "Good morning, sunshine!",
+    "Enjoy your day!",
+    "How was your sleep?"
+  ],
+  "afternoon": [
+    "Hitting your stride!",
+    "You are making a difference!",
+    "You're more fun than bubble wrap!",
+    "Looking good today!",
+    "Hello beauty!"
+  ],
+  "evening": [
+    "You made someone smile today, I know it.",
+    "You are making a difference.",
+    "The day was better for your efforts."
+  ],
+  "....-01-01": ["Happy new year!"],
+  "....-12-25": ["Merry Christmas!"],
+  "....-02-14": ["Happy Valentine's Day!"],
+  "....-02-02": [
+    "Okay campers, rise and shine",
+    "Don't forget your booties because it's cold out there today",
+    "It's cold out there every day.",
+    "What is this, Miami Beach?",
+    "That's right woodchuck chuckers. It's Groundhog day!"
+  ],
+  "day_sunny": [
+    "Today is a sunny day",
+    "It's a beautiful day"
+  ],
+  "rain": ["Don't forget your umbrella"],
+  "thunderstorm": ["It's  going to thunder"],
+  "night_thunderstorm": ["It's going to thunder tonight"],
+  "night_rain": ["It's going to rain tonight"],
+}
